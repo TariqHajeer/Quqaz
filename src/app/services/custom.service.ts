f6cc3b2b19ef1a80671c0ec5919bcfebe2e69f7d
tareqhajer <tareqhajer@gmail.com>
2021-01-11 08:08:38 +0200

    add region

--- src/app/services/custom.service.ts
+++ src/app/services/custom.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
-import {environment} from '../../environments/environment.prod'
+import { environment } from '../../environments/environment.prod'
 import { Observable } from 'rxjs';
-import { map,tap, catchError } from 'rxjs/operators';
+import { map, tap, catchError } from 'rxjs/operators';
 import { ConditionalExpr } from '@angular/compiler';
 import { redirectLoggedInTo } from '@angular/fire/auth-guard';
 @Injectable({
@@ -13,113 +13,117 @@ import { redirectLoggedInTo } from '@angular/fire/auth-guard';
 export class CustomService {
 
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
   baseUrl: string = environment.baseUrl;
 
-    addOrUpdate(apiName,item,status,id?):Observable<any>{
-    let headers=new HttpHeaders({'Content-Type':'multipart/form-data'});
-    if(status=='add')
-    return this.http.post(this.baseUrl+'api/'+apiName,item);
-    else if(status=='update')
-    return this.http.patch(this.baseUrl+'api/'+apiName,item);
+  addOrUpdate(apiName, item, status, id?): Observable<any> {
+    let headers = new HttpHeaders({ 'Content-Type': 'multipart/form-data' });
+    if (status == 'add')
+      return this.http.post(this.baseUrl + 'api/' + apiName, item);
+    else if (status == 'update')
+      return this.http.patch(this.baseUrl + 'api/' + apiName, item);
   }
-  exitsstudent(newstudent):Observable<any>{
-    let item=new FormData();
-    item.append('firstName',newstudent.firstName);
-    item.append('middleName',newstudent.middleName);
-    item.append('lastName',newstudent.lastName);
-    item.append('mobileNumber',newstudent.mobileNumber);
-    item.append('phoneNumber',newstudent.phoneNumber);
-    return this.http.post(this.baseUrl+'api/StudentExist',item);
+  Create(apiName, item): Observable<any> {
+    return this.http.post(this.baseUrl + 'api/' + apiName, item);
+  }
+  exitsstudent(newstudent): Observable<any> {
+    let item = new FormData();
+    item.append('firstName', newstudent.firstName);
+    item.append('middleName', newstudent.middleName);
+    item.append('lastName', newstudent.lastName);
+    item.append('mobileNumber', newstudent.mobileNumber);
+    item.append('phoneNumber', newstudent.phoneNumber);
+    return this.http.post(this.baseUrl + 'api/StudentExist', item);
 
   }
-  delete(apiName,id):Observable<any>
-  {
-    return this.http.delete(this.baseUrl+'api/'+apiName+'/'+id);
+  delete(apiName, id): Observable<any> {
+    return this.http.delete(this.baseUrl + 'api/' + apiName + '/' + id);
   }
-  getAll(apiName):Observable<any>{
-    return this.http.get(this.baseUrl+'api/'+apiName);
+  getAll(apiName): Observable<any> {
+    return this.http.get(this.baseUrl + 'api/' + apiName);
   }
-  getAllsy(apiName):Observable<[]>{
-    return this.http.get(this.baseUrl+'api/'+apiName).pipe(
+  getAllsy(apiName): Observable<[]> {
+    return this.http.get(this.baseUrl + 'api/' + apiName).pipe(
       map(
-        (res:any)=>{
+        (res: any) => {
           console.log('pipe');
           console.log(res);
-          let a:[]=[];
-          a=res.data[0];
+          let a: [] = [];
+          a = res.data[0];
           return a;
         }
       )
     );
   }
-  getById(apiName,id):Observable<any>{
-    let headers=new HttpHeaders({'Content-Type':'application/json'});
-    return this.http.get<any>(this.baseUrl+'api/'+apiName+'/'+id,{headers});
+  getById(apiName, id): Observable<any> {
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.get<any>(this.baseUrl + 'api/' + apiName + '/' + id, { headers });
   }
 
-  getById2(apiName1,id,apiName2):Observable<any>{
-    let headers=new HttpHeaders({'Content-Type':'application/json'});
-    return this.http.get<any>(this.baseUrl+'api/'+apiName1+'/'+id+'/'+apiName2,{headers});
+  getById2(apiName1, id, apiName2): Observable<any> {
+    let headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.get<any>(this.baseUrl + 'api/' + apiName1 + '/' + id + '/' + apiName2, { headers });
   }
-  getWithParam(apiName,param:any[]):Observable<any>{
+  getWithParam(apiName, param: any[]): Observable<any> {
     let params = new HttpParams();
-    for(let p of param){
-    params = params.append(p.k, p.v);
+    for (let p of param) {
+      params = params.append(p.k, p.v);
     }
-   return this.http.get(this.baseUrl+'api/'+apiName,{params:params});
+    return this.http.get(this.baseUrl + 'api/' + apiName, { params: params });
   }
-  addWithParam(apiName,param:any[]):Observable<any>{
+  addWithParam(apiName, param: any[]): Observable<any> {
     let params = new HttpParams();
-    for(let p of param){
-    params = params.append(p.k, p.v);
+    for (let p of param) {
+      params = params.append(p.k, p.v);
     }
-    return this.http.post(this.baseUrl+'api/'+apiName,{},{params:params});
+    return this.http.post(this.baseUrl + 'api/' + apiName, {}, { params: params });
   }
 
-  sendmessage(message,title,member,file,length):Observable<any>{
+  sendmessage(message, title, member, file, length): Observable<any> {
     console.log(member);
     console.log(message);
     console.log(title);
     console.log(member);
     console.log(file);
-    const form=new FormData();
+    const form = new FormData();
 
-    for (let i = 0;i<length; i++){
+    for (let i = 0; i < length; i++) {
       //select type student
-    if(member[i].sender == 'students'){
-    form.append('loginable_type_id['+i+']','Student');
-    // is_class true
-    if(member[i].all_class == true){
-      form.append('is_all_section['+i+']','0');
-      form.append('is_all_student['+i+']','0');
-      form.append('is_all_class['+i+']','1');
-    }
-    // is_class false
-    else {
-      form.append('is_all_class['+i+']','0');
-      form.append('class_id['+i+'][0]',member[i].class_id.grade_name.id);
-      // is_section true
-    if(member[i].all_section == true){form.append('is_all_section['+i+']','1');        form.append('is_all_student['+i+']','0');
+      if (member[i].sender == 'students') {
+        form.append('loginable_type_id[' + i + ']', 'Student');
+        // is_class true
+        if (member[i].all_class == true) {
+          form.append('is_all_section[' + i + ']', '0');
+          form.append('is_all_student[' + i + ']', '0');
+          form.append('is_all_class[' + i + ']', '1');
+        }
+        // is_class false
+        else {
+          form.append('is_all_class[' + i + ']', '0');
+          form.append('class_id[' + i + '][0]', member[i].class_id.grade_name.id);
+          // is_section true
+          if (member[i].all_section == true) {
+            form.append('is_all_section[' + i + ']', '1'); form.append('is_all_student[' + i + ']', '0');
 
-  }
-    // is_section false
-    else{
-      form.append('is_all_section['+i+']','0');
-      form.append('section_id['+i+'][0]',member[i].section.id);
-      // is_student true
-      if(member[i].all_student == true){        form.append('is_all_student['+i+']','1');
-    }
-      // is_student false
-      else {
-        form.append('is_all_student['+i+']','0');
-        form.append('student_id['+i+'][0]',member[i].student.id);
+          }
+          // is_section false
+          else {
+            form.append('is_all_section[' + i + ']', '0');
+            form.append('section_id[' + i + '][0]', member[i].section.id);
+            // is_student true
+            if (member[i].all_student == true) {
+              form.append('is_all_student[' + i + ']', '1');
+            }
+            // is_student false
+            else {
+              form.append('is_all_student[' + i + ']', '0');
+              form.append('student_id[' + i + '][0]', member[i].student.id);
+            }
+          }
+        }
       }
-    }
-    }
-    }
-    /// end select type
-    else if(member.sender == 'Staff' ){}
+      /// end select type
+      else if (member.sender == 'Staff') { }
     }
 
 
@@ -128,39 +132,39 @@ export class CustomService {
     // form.append('is_all_class[0]','1');
     // form.append('is_all_student[0]','1');
     // form.append('is_all_section[0]','1');
-    form.append('title',title);
-    form.append('content',message);
-    for(let i = 0 ;i<file.file.length;i++){
-    form.append('file_name['+i+']',file.file[i].File);
+    form.append('title', title);
+    form.append('content', message);
+    for (let i = 0; i < file.file.length; i++) {
+      form.append('file_name[' + i + ']', file.file[i].File);
     }
 
 
-    return this.http.post(this.baseUrl+'api'+'/sendMessageToAudiences',form);
+    return this.http.post(this.baseUrl + 'api' + '/sendMessageToAudiences', form);
   }
 
 
 
-  disable(id,a):Observable<any>{
+  disable(id, a): Observable<any> {
     console.log(a);
-    let form=new FormData();
-    form.append('is_disabled',a);
-    return this.http.post(this.baseUrl+'api/disabledStudent/'+id,form);
+    let form = new FormData();
+    form.append('is_disabled', a);
+    return this.http.post(this.baseUrl + 'api/disabledStudent/' + id, form);
   }
 
 
   getexambyid(id): Observable<any> {
-    return this.http.get(this.baseUrl+'api'+'/Exam/'+id);
+    return this.http.get(this.baseUrl + 'api' + '/Exam/' + id);
 
   }
 
-  acceptmessage(id):Observable<any>{
-    return this.http.post(this.baseUrl+'api'+'/acceptMessage/'+id,{});
+  acceptmessage(id): Observable<any> {
+    return this.http.post(this.baseUrl + 'api' + '/acceptMessage/' + id, {});
   }
-  archivemessage(id):Observable<any>{
-    return this.http.post(this.baseUrl+'api'+'/archiveMessage/'+id,{});
+  archivemessage(id): Observable<any> {
+    return this.http.post(this.baseUrl + 'api' + '/archiveMessage/' + id, {});
   }
-  deletemessage(id):Observable<any>{
-    return this.http.post(this.baseUrl+'api'+'/deleteMessageFromMe/'+id,{});
+  deletemessage(id): Observable<any> {
+    return this.http.post(this.baseUrl + 'api' + '/deleteMessageFromMe/' + id, {});
   }
 
 }
--- src/app/views/app/setting/region/region.component.ts
+++ src/app/views/app/setting/region/region.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { EditSettingsModel, GridComponent, IEditCell, SaveEventArgs, toogleCheckbox, ToolbarItems } from '@syncfusion/ej2-angular-grids';
+import { ActionEventArgs, EditSettingsModel, GridComponent, IEditCell, SaveEventArgs, toogleCheckbox, ToolbarItems } from '@syncfusion/ej2-angular-grids';
 import { NotificationsService, NotificationType } from 'angular2-notifications';
 import { City } from 'src/app/Models/Cities/city.Model';
 import { CustomService } from 'src/app/services/custom.service';
@@ -25,6 +25,7 @@ export class RegionComponent implements OnInit {
   public toolbar: Object[];
   public pageSettings: Object;
   public citiesParameter: IEditCell;
+  apiName: string = "Region";
   ngOnInit(): void {
     this.getRegions();
     this.editSettings = { showDeleteConfirmDialog: false, allowAdding: true, allowEditing: true, allowEditOnDblClick: true, allowDeleting: true };
@@ -44,11 +45,40 @@ export class RegionComponent implements OnInit {
     this.citiesParameter = {
       params: {
         allowFiltering: true,
-        dataSource: new DataManager({url:"https://localhost:44333/api/Country"}),
-        fields: { text: 'name', value: 'name' },
-         actionComplete: () => false
+        dataSource: new DataManager({ url: "https://localhost:44333/api/Country" }),
+        fields: { text: 'name', value: 'id' },
+        actionComplete: () => false
       }
     };
+  }
+  onActionBegin(args: ActionEventArgs) {
+    if (args.action == 'add') {
+      if (args.requestType == "save") {
+        if (args.data['name'] == undefined || args.data['name'] == "") {
+          this.notifications.create('', 'الأسم فارغ', NotificationType.Warn, { timeOut: 6000, showProgressBar: false });
+          args.cancel = true;
+        }
+
+        let name = args.data["name"].trim();
+        let countryId = args.data['country']['name'];
+        if (countryId == null) {
+          args.cancel = true;
+          this.notifications.create('', 'يجب اختيار المدينة', NotificationType.Warn, { timeOut: 6000, showProgressBar: false });
+        }
+        console.log(this.regions.filter(c => c.name == name).length);
+        if (this.regions.filter(c => c.name == name && c.country.id == countryId).length != 0) {
+          this.notifications.create('', 'الاسم مكرر', NotificationType.Warn, { timeOut: 6000, showProgressBar: false });
+          args.cancel = true;
+        } else {
+          args.cancel = true;
+          this.customService.Create(this.apiName,{name:name,countryId:countryId}).toPromise().then(res=>{
+            this.notifications.create('', 'تم اضافة نوع صادرات بنجاح', NotificationType.Success, { theClass: 'success', timeOut: 6000, showProgressBar: false });
+            this.regions.push(res);
+            this.gridInstance.refresh();
+          });
+        }
+      }
+    }
   }
   load() {
     const rowHeight: number = this.gridInstance.getRowHeight();  // height of the each row
