448e86b181d1bff60cc5a01e71c20a6fd3bea563
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-01-19 05:31:06 +0200

    add

--- src/app/views/app/order/add-orders/add-orders.component.ts
+++ src/app/views/app/order/add-orders/add-orders.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { City } from 'src/app/Models/Cities/city.Model';
 import { NameAndIdDto } from 'src/app/Models/name-and-id-dto.model';
-<<<<<<< HEAD
 import { CreateOrdersFromEmployee, OrderItem } from 'src/app/Models/order/create-orders-from-employee.model';
 import { Order } from 'src/app/Models/order/order.model';
 import { OrderType } from 'src/app/Models/OrderTypes/order-type.model';
-=======
-import { CreateOrdersFromEmployee } from 'src/app/Models/order/create-orders-from-employee.model';
->>>>>>> f2203a99893bd9f09ff74ca075f88cf6bd16859c
+
 import { Region } from 'src/app/Models/Regions/region.model';
 import { User } from 'src/app/Models/user/user.model';
 import { CustomService } from 'src/app/services/custom.service';
@@ -24,20 +21,16 @@ import { ClientService } from '../../client/client.service';
 export class AddOrdersComponent implements OnInit {
 
   constructor(private orderservice: OrderService,
-<<<<<<< HEAD
+
     private clientService: ClientService
     , private customerService: CustomService,
     private userService: UserService) { }
-=======
-    private clientService:ClientService
-    ,private customerService:CustomService,
-    private userService:UserService) { }
->>>>>>> f2203a99893bd9f09ff74ca075f88cf6bd16859c
+
   Order: CreateOrdersFromEmployee
   submitted = false;
   orderPlace: NameAndIdDto[] = []
   MoenyPlaced: NameAndIdDto[] = []
-<<<<<<< HEAD
+
   clients: Client[] = []
   cities: City[] = []
   Region: Region[] = []
@@ -54,22 +47,13 @@ export class AddOrdersComponent implements OnInit {
     this.Order.OrderTypeDtos=[]
     this.orderType=new OrderType
     this.OrderItem= new OrderItem
-=======
-  clients:Client[]=[]
-  cities:City[]=[]
-  Region: Region[]=[]
-  Agents:User[]=[]
-  cityapi="Country"
-  regionapi="Region"
-  ngOnInit(): void {
-    this.Order = new CreateOrdersFromEmployee
->>>>>>> f2203a99893bd9f09ff74ca075f88cf6bd16859c
+
     this.GetMoenyPlaced()
     this.GetorderPlace()
     this.GetRegion()
     this.Getcities()
     this.GetClient()
-<<<<<<< HEAD
+
     this.getOrderTypes()
     this.userService.GetAll()
     this.Agents = this.userService.users
@@ -118,38 +102,7 @@ export class AddOrdersComponent implements OnInit {
     this.OrderItem.Count = this.count
     this.Order.OrderTypeDtos.push(this.OrderItem)
 
-=======
-    this.userService.GetAll()
-    this.Agents=this.userService.users
->>>>>>> f2203a99893bd9f09ff74ca075f88cf6bd16859c
   }
-  AddOrder() {
-
-  }
-  GetorderPlace() {
-    this.orderservice.orderPlace().subscribe(res => {
-      this.orderPlace = res
-    })
-  }
-  GetMoenyPlaced() {
-    this.orderservice.MoenyPlaced().subscribe(res => {
-      this.MoenyPlaced = res
-    })
-  }
-  GetClient(){
-    this.clientService.getClients().subscribe(res=>{
-      this.clients=res
-    })
-  }
-  Getcities(){
-    this.customerService.getAll(this.cityapi).subscribe(res=>{
-      this.cities=res
-    })
-  }
-  GetRegion(){
-    this.customerService.getAll(this.regionapi).subscribe(res=>{
-      this.Region=res
-    })
-  }
+ 
  
 }
