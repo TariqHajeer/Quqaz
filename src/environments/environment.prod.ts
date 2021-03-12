37ac764c1624219aae136ba7710b88c10e07f56d
tareqhajer <tareqhajer@gmail.com>
2021-03-12 19:12:37 +0200

    online

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 import { UserPermission } from '../app/shared/auth.roles';
 
 export const environment = {
-//baseUrl:"http://tariqhajeer2-001-site1.etempurl.com/",
-   baseUrl:"https://localhost:44333/",
- // baseUrl: 'http://tariqhajeer-001-site1.etempurl.com/',
+  baseUrl: "http://tariqhajeer2-001-site1.etempurl.com/",
+  //baseUrl:"https://localhost:44333/",
+  // baseUrl: 'http://tariqhajeer-001-site1.etempurl.com/',
   production: true,
   // buyUrl: "http://tariqhajeer-001-site1.etempurl.com/",
   SCARF_ANALYTICS: false,
