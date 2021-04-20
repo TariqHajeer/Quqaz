dffb84b7853bc7798774c2adf29c392996334976
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-04-20 16:51:09 +0300

    new edit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 import { UserPermission } from '../app/shared/auth.roles';
 
 export const environment = {
-  //baseUrl: "http://tariqhajeer2-001-site1.etempurl.com/",
-  baseUrl:"https://localhost:44333/",
+  baseUrl: "http://tariqhajeer2-001-site1.etempurl.com/",
+  //baseUrl:"https://localhost:44333/",
   production: true,
   SCARF_ANALYTICS: false,
   adminRoot: '/app',
