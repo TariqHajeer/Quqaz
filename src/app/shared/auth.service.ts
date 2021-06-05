8fb1a7033124d9ff26013f1a3d9df33ef218aa8e
tareqhajer <tareqhajer@gmail.com>
2021-06-05 09:59:27 +0300

    new edit ..... expire to 14 h

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -99,7 +99,7 @@ export class AuthService implements OnDestroy{
     if (!accessToken) {
       return 0;
     }
-    return 1 * 60 * 60 * 1000*12
+    return 1 * 60 * 60 * 1000*14
   }
   public startTokenTimer() {
     const timeout = this.getTokenRemainingTime()
