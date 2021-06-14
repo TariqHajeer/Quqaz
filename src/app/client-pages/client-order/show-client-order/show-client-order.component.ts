50302449c6d351b3fed1f82fc4fd374310af59d2
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-06-14 06:58:57 +0300

    client pages/addorder

--- src/app/client-pages/client-order/show-client-order/show-client-order.component.ts
+++ src/app/client-pages/client-order/show-client-order/show-client-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-show-client-order',
@@ -7,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowClientOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 addorder(){
-  
+this.router.navigate(['/clienthome/orders/addorder'])
 }
 }
