f3ce57f8db18cd3c17a470c65ad2ab14c2a1b5ba
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-06-22 17:21:46 +0300

    client orders

--- src/app/client-pages/client-pages.component.ts
+++ src/app/client-pages/client-pages.component.ts
@@ -15,49 +15,7 @@ export class ClientPagesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  @HostListener("window:resize", ["$event"])
-  onResize(event) {
-    const homeRect = this.elRef.nativeElement
-      .querySelector(".home-row")
-      .getBoundingClientRect();
-
-    const homeSection = this.elRef.nativeElement.querySelector(
-      ".landing-page .section.home"
-    );
-    homeSection.style.backgroundPositionX = homeRect.x - 580 + "px";
-
-    const footerSection = this.elRef.nativeElement.querySelector(
-      ".landing-page .section.footer"
-    );
-    footerSection.style.backgroundPositionX = event.target.innerWidth - homeRect.x - 2000 + "px";
-
-    if (event.target.innerWidth >= 992) {
-      this.renderer.removeClass(
-        this.elRef.nativeElement.querySelector(".landing-page"),
-        "show-mobile-menu"
-      );
-    }
-  }
-  showMobileMenu = false;
-
-  @HostListener("window:click", ["$event"])
-  onClick(event) {
-    this.showMobileMenu = false;
-  }
-
-  @HostListener("window:scroll", ["$event"])
-  onScroll(event) {
-    this.showMobileMenu = false;
-  }
-
-  scrollTo(target) {
-    const config: ScrollToConfigOptions = {
-      target,
-      offset: -150
-    };
-
-    this.scrollToService.scrollTo(config);
-  }
+  
   login() {
     this.router.navigate(['/login'])
   }
