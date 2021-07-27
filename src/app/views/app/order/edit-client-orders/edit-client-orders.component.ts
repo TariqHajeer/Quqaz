62a17c4ffe3633c07b62a8d8d9afef10c2d0036e
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-07-27 12:08:11 +0300

    can edit client orders

--- src/app/views/app/order/edit-client-orders/edit-client-orders.component.ts
+++ src/app/views/app/order/edit-client-orders/edit-client-orders.component.ts
@@ -5,6 +5,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Order } from 'src/app/Models/order/order.model';
 import { MatTableDataSource } from '@angular/material/table';
+import { ClientService } from '../../client/client.service';
+import { Client } from '../../client/client.model';
+import { NotificationsService, NotificationType } from 'angular2-notifications';
 @Component({
   selector: 'app-edit-client-orders',
   templateUrl: './edit-client-orders.component.html',
@@ -12,10 +15,13 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class EditClientOrdersComponent implements OnInit {
 
-  constructor(private editrequestService: EditRequestService) { }
+  constructor(private editrequestService: EditRequestService,
+    private clientService: ClientService,
+    private notifications: NotificationsService,) { }
 
   ngOnInit(): void {
     this.Get()
+    this.getClient()
   }
   displayedColumns: string[];
   dataSource
@@ -23,28 +29,64 @@ export class EditClientOrdersComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   noDataFound: boolean = false
   editRequest: EditRequest[] = []
+  clients: Client[] = [];
+  nameIsRepeated: boolean = false;
+  usernameIsRepeated: boolean = false;
   Get() {
     this.editrequestService.NewEditReuqet().subscribe(res => {
       console.log(res)
-      this.editRequest=res
+      this.editRequest = res
       if (this.editRequest.length == 0)
-      this.noDataFound = true
-    else this.noDataFound = false
-    this.dataSource = new MatTableDataSource(this.editRequest);
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.displayedColumns = ['client','oldName', 'newName', 'oldUserName',
-      'newUserName',  'Accept', 'DisAccept'];
+        this.noDataFound = true
+      else this.noDataFound = false
+      this.dataSource = new MatTableDataSource(this.editRequest);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.displayedColumns = ['client', 'oldName', 'newName', 'oldUserName',
+        'newUserName', 'Accept', 'DisAccept'];
     })
   }
-  Accpet(id){
-    this.editrequestService.Accpet(id).subscribe(res => {
+  Accpet(element: EditRequest) {
+    if (this.checkName(element.newName) && element.newName != element.oldName) {
+      this.notifications.create('error', 'الاسم مكرر', NotificationType.Error, { theClass: 'success', timeOut: 6000, showProgressBar: false });
+      this.nameIsRepeated=true
+    }
+    else
+    this.nameIsRepeated=false
+    if (this.checkUserName(element.newUserName) && element.oldUserName != element.newUserName) {
+      this.notifications.create('error', 'اسم المستخدم مكرر', NotificationType.Error, { theClass: 'success', timeOut: 6000, showProgressBar: false });
+      this.usernameIsRepeated=true
+    }
+    else
+    this.usernameIsRepeated=false
+    if(  this.nameIsRepeated||  this.usernameIsRepeated)return
+    else
+    this.editrequestService.Accpet(element.id).subscribe(res => {
      this.Get()
     })
   }
-  DisAccpet(id){
+  DisAccpet(id) {
     this.editrequestService.DisAccpet(id).subscribe(res => {
       this.Get()
     })
+  }
+  getClient() {
+    this.clientService.getClients().subscribe(
+      res => {
+        this.clients = res
+      }
+    )
+  }
+  checkName(name): boolean {
+    if (this.clients.filter(c => c.name == name).length > 0) {
+      return true
+    } else
+      return false
+  }
+  checkUserName(userName): boolean {
+    if (this.clients.filter(c => c.userName == userName).length > 0) {
+      return true
+    } else
+      return false
   }
 }
