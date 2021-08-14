4595ad36c77a0131ac4228b3874c60c0077e3636
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-08-14 17:30:41 +0300

    address

--- src/app/views/app/reports/printpreview/agent/agent.component.ts
+++ src/app/views/app/reports/printpreview/agent/agent.component.ts
@@ -39,7 +39,7 @@ export class AgentComponent implements OnInit {
     this.PrintNumberOrder = new PrintNumberOrder
     this.orders = JSON.parse(localStorage.getItem('printordersagent'))
     this.orders=this.orders.sort((a,b)=>a.code-b.code)
-    console.log(  this.orders )
+    // console.log(  this.orders )
     this.agent = JSON.parse(localStorage.getItem('printagent'))
     this.orderplaced = this.orders.map(o => o.orderplaced)[0]
     this.sumCost()
