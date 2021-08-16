85fd06897bb20bae22c2442e217c9b235d517fbc
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-08-16 17:30:38 +0300

    remove all photo and commponent not be used

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'clienthome', loadChildren: () => 
-  import('./client-pages/client-pages.module').then(m => m.ClientPagesModule) },
+  // { path: 'clienthome', loadChildren: () => 
+  // import('./client-pages/client-pages.module').then(m => m.ClientPagesModule) },
   { path: 'view', loadChildren: () =>
    import('./views/views.module').then(m => m.ViewsModule) },
   //  {
