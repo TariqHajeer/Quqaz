ce7c4b5d9d18f487865cb12850c3eee677380871
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-10-12 08:04:03 +0300

    print

--- src/app/views/app/agent/order-suspended/order-suspended.component.ts
+++ src/app/views/app/agent/order-suspended/order-suspended.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+import { City } from 'src/app/Models/Cities/city.Model';
+import { DateWithIds } from 'src/app/Models/date-with-ids.model';
+import { Order, IdsDto } from 'src/app/Models/order/order.model';
+import { User } from 'src/app/Models/user/user.model';
+import { AgentOrderService } from 'src/app/services/agent-order.service';
+import { CustomService } from 'src/app/services/custom.service';
+import { Client } from '../../client/client.model';
+import { ClientService } from '../../client/client.service';
 
 @Component({
   selector: 'app-order-suspended',
@@ -7,9 +19,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSuspendedComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[];
+  dataSource
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  orders: Order[] = []
+  noDataFound: boolean = false
+
+  constructor(private OrderService: AgentOrderService,) { }
+  @ViewChild('infoModal') public infoModal: ModalDirective;
 
   ngOnInit(): void {
+    this.get()
+   
+    // this.order = new Order()
   }
+  // ngOnChanges() {
+  //   this.print()
+  // }
+
+  get() {
+    this.OrderService.OrderSuspended().subscribe(res => {
+      // console.log(res)
+      this.orders = res
+      this.orders.forEach(res => {
+        res.recipientPhones = res.recipientPhones.split(',')
+      })
+      if (this.orders.length == 0)
+        this.noDataFound = true
+      else this.noDataFound = false
+      this.dataSource = new MatTableDataSource(this.orders);
+      this.dataSource.sort = this.sort;
+      this.dataSource.paginator = this.paginator;
+      this.displayedColumns = ['code', 'cost', 'recipientName',
+        'recipientPhones', 'address', 'note', 'client', 'country'
+        , 'region', 'date'];
+    })
+
+  }
+  order: Order = new Order
+  AgentId
+  Agents: User[] = []
+  IdsDto: IdsDto = new IdsDto
+ 
+ 
 
 }
