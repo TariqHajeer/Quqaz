e8ec0aa5945f6b1a1d03cdd2dd381573a0d8ef7f
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-01-05 21:46:28 +0200

    delete-commit-inside-envirement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -41,11 +41,6 @@ export const environment = {
   menuHiddenBreakpoint: 768,
   themeColorStorageKey: 'vien-themecolor',
   isMultiColorActive: true,
-  /*
-  Color Options:
-  'light.blueyale', 'light.blueolympic', 'light.bluenavy', 'light.greenmoss', 'light.greenlime', 'light.yellowgranola', 'light.greysteel', 'light.orangecarrot', 'light.redruby', 'light.purplemonster'
-  'dark.blueyale', 'dark.blueolympic', 'dark.bluenavy', 'dark.greenmoss', 'dark.greenlime', 'dark.yellowgranola', 'dark.greysteel', 'dark.orangecarrot', 'dark.redruby', 'dark.purplemonster'
-  */
   defaultColor: 'light.blueyale',
   isDarkSwitchActive: true,
   defaultDirection: 'rtl',
