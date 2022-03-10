30d5671bf5d434d7cf8f0c7f8c69bf4dd65b284b
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-03-10 13:37:52 +0200

    GetOrderForPayBy

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -260,10 +260,7 @@ export class OrderService {
     return this.http.get(this.controler + "OrderVicdanAgent/" + AgentId)
   }
   OrderInCompany(ClientId, code) {
-    return this.http.get(this.controler + "OrderInCompany/" + ClientId + "/" + code)
-  }
-  OrderInCompanyv2(ClientId, code) {
-    return this.http.get(this.controler + "OrderInCompanyv2/" + ClientId + "/" + code)
+    return this.http.get(this.controler + "GetOrderForPayBy/" + ClientId + "/" + code)
   }
   DeleiverMoneyForClientWithStatus(ids) {
     return this.http.put<any>(this.controler + "DeleiverMoneyForClientWithStatus", ids)
