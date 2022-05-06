f7790279bd412210653531542a3d704f9b54ad1d
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-05-06 21:23:52 +0300

    create component

---
+++ src/app/views/app/reports/receiptf-receiving-shipment/receiptf-receiving-shipment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receiptf-receiving-shipment',
+  templateUrl: './receiptf-receiving-shipment.component.html',
+  styleUrls: ['./receiptf-receiving-shipment.component.scss']
+})
+export class ReceiptfReceivingShipmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
