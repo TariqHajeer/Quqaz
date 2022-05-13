824138d07dfe177ecf522151dfe0c95cbb366f53
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-05-13 14:31:16 +0300

    haveTreasury

--- src/app/Models/userlogin.model.ts
+++ src/app/Models/userlogin.model.ts
@@ -4,4 +4,5 @@ export class UserLogin {
     privileges:any[]
     expiry:any
     policy:any
+    haveTreasury:boolean
 }
--- src/app/constants/menu.ts
+++ src/app/constants/menu.ts
@@ -12,6 +12,7 @@ export interface IMenuItem {
   permission?: UserPermission[];
   badge?: boolean
   badgeLable?
+  enabled?:boolean;
 }
 
 const data: IMenuItem[] = [
