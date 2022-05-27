16afd43b6b021a13c25271bc2850c335bf77a4a2
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-05-27 19:59:35 +0300

    target=_blank

--- src/app/Models/user/treasury.model.ts
+++ src/app/Models/user/treasury.model.ts
@@ -1,20 +1,21 @@
 export class Treasury {
-    id: number;
+  id: number;
+  total: number;
+  createOnUtc: Date;
+  userName: string;
+  isActive: boolean;
+  history: {
+    data: TreasuryHistory[];
     total: number;
-    createOnUtc: Date;
-    isActive: boolean;
-    history: {
-        data: TreasuryHistory[],
-        total: number
-    };
+  };
 }
 export class TreasuryHistory {
-    amount: number;
-    type: string;
-    createdOnUtc: Date;
-    clientPaymentId: number;
-    cashMovmentId: number;
-    receiptId: number;
-    incomeId: number;
-    outcomeId: number;
+  amount: number;
+  type: string;
+  createdOnUtc: Date;
+  clientPaymentId: number;
+  cashMovmentId: number;
+  receiptId: number;
+  incomeId: number;
+  outcomeId: number;
 }
