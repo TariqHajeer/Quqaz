347f60fe4746b1cea716e632ac85f7efd5081e3e
BayanALmouslly <bayanmsl.bm@gmail.com>
2022-08-19 07:15:00 +0300

    add ConvertToExcelService to SetPrintNumberComponent

---
+++ src/app/services/convert-to-excel.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+
+const EXCEL_TYPE =
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+@Injectable({
+  providedIn: 'root',
+})
+export class ConvertToExcelService {
+  constructor() {}
+
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { data: worksheet },
+      SheetNames: ['data'],
+    };
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+  private saveAsExcelFile(buffer: any, fileName: string): void {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    saveAs.saveAs(
+      data,
+      fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION
+    );
+  }
+}
--- src/app/views/app/reports/printpreview/set-print-number/set-print-number.component.ts
+++ src/app/views/app/reports/printpreview/set-print-number/set-print-number.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectorRef, Component, HostListener, Input, OnChanges, OnInit } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { NotificationsService, NotificationType } from 'angular2-notifications';
 import { UserLogin } from 'src/app/Models/userlogin.model';
@@ -10,106 +17,124 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { DateService } from 'src/app/services/date.service';
+import { ConvertToExcelService } from 'src/app/services/convert-to-excel.service';
 @Component({
   selector: 'app-set-print-number',
   templateUrl: './set-print-number.component.html',
-  styleUrls: ['./set-print-number.component.scss']
+  styleUrls: ['./set-print-number.component.scss'],
 })
 export class SetPrintNumberComponent implements OnInit {
-
-  constructor(private orderservice: OrderService,
+  constructor(
+    private orderservice: OrderService,
     private notifications: NotificationsService,
     public sanitizer: DomSanitizer,
     private cdr: ChangeDetectorRef,
     private spinner: NgxSpinnerService,
     public getroute: ActivatedRoute,
-    public dateService: DateService
+    public dateService: DateService,
+    public convertToExcelService: ConvertToExcelService
+  ) {}
+  heads = [
+    'ترقيم',
+    'كود',
+    'الإجمالي',
+    'التاريخ',
+    'المحافظة ',
+    'المنطقة / العنوان  ',
+    'الهاتف',
+    'اسم العميل',
+    'ملاحظات العميل',
+    'مـلاحظـــــات',
+  ];
 
-
-  ) { }
-  heads = ['ترقيم', 'كود', 'الإجمالي','التاريخ', 'المحافظة ','المنطقة / العنوان  ', 'الهاتف', 'اسم العميل','ملاحظات العميل', 'مـلاحظـــــات']
-
-  orders: any[] = []
-  count = 0
-  agent
-  dateOfPrint = new Date()
-  userName
-  printnumber
-  PrintNumberOrder: PrintNumberOrder
-  address = environment.Address
-  companyPhone = environment.companyPhones[0]+" - "+ environment.companyPhones[1]
+  orders: any[] = [];
+  count = 0;
+  agent;
+  dateOfPrint = new Date();
+  userName;
+  printnumber;
+  PrintNumberOrder: PrintNumberOrder;
+  address = environment.Address;
+  companyPhone =
+    environment.companyPhones[0] + ' - ' + environment.companyPhones[1];
   ngOnInit(): void {
-this.changeDeleiverMoneyForClient()
+    this.changeDeleiverMoneyForClient();
   }
 
   sumCost() {
-    this.count = 0
+    this.count = 0;
     if (this.orders)
-      this.orders.forEach(o => {
-        this.count += o.total
-      })
-    return this.count
+      this.orders.forEach((o) => {
+        this.count += o.total;
+      });
+    return this.count;
   }
 
-  showPrintbtn = false
-  phones
+  showPrintbtn = false;
+  phones;
   changeDeleiverMoneyForClient() {
-    this.getroute.params.subscribe(par => {
-      this.printnumber = par['printnumber'] as any
+    this.getroute.params.subscribe((par) => {
+      this.printnumber = par['printnumber'] as any;
     });
-    this.spinner.show()
-    this.orderservice.GetOrderByAgnetPrintNumber(this.printnumber).subscribe(res => {
-      console.log(res)
-      this.spinner.hide()
-      this.showPrintbtn = true
-      this.orders = res.orders
-      this.orders=this.orders.sort((a,b)=>a.code-b.code)
-      var address=""
-    for(let i=0;i<this.orders.length;i++){
-      var space=0
-      if(this.orders[i].address)
-      for(let j=0;j<this.orders[i].address.length;j++){
-        address+= this.orders[i].address[j]
-      if(this.orders[i].address[j]==" ")
-      space++
-      if(space==2){
-        this.orders[i].address=address
-        address=""
-        break
+    this.spinner.show();
+    this.orderservice.GetOrderByAgnetPrintNumber(this.printnumber).subscribe(
+      (res) => {
+        console.log(res);
+        this.spinner.hide();
+        this.showPrintbtn = true;
+        this.orders = res.orders;
+        this.orders = this.orders.sort((a, b) => a.code - b.code);
+        var address = '';
+        for (let i = 0; i < this.orders.length; i++) {
+          var space = 0;
+          if (this.orders[i].address)
+            for (let j = 0; j < this.orders[i].address.length; j++) {
+              address += this.orders[i].address[j];
+              if (this.orders[i].address[j] == ' ') space++;
+              if (space == 2) {
+                this.orders[i].address = address;
+                address = '';
+                break;
+              }
+            }
+        }
+        this.agent = res.destinationName;
+        this.phones = res.destinationPhone;
+        this.printnumber = res.printNmber;
+        this.dateOfPrint = res.date;
+        this.userName = res.printerName;
+        this.sumCost();
+      },
+      (err) => {
+        this.spinner.hide();
+        this.showPrintbtn = false;
+        this.notifications.create(
+          'success',
+          'رقم الطباعة غير موجود',
+          NotificationType.Error,
+          { theClass: 'success', timeOut: 6000, showProgressBar: false }
+        );
       }
-      } 
-    }
-      this.agent = res.destinationName
-      this.phones = res.destinationPhone
-      this.printnumber = res.printNmber
-      this.dateOfPrint = res.date
-      this.userName = res.printerName
-      this.sumCost()
-    }, err => {
-      this.spinner.hide()
-      this.showPrintbtn = false
-      this.notifications.create('success', 'رقم الطباعة غير موجود', NotificationType.Error, { theClass: 'success', timeOut: 6000, showProgressBar: false });
-
-    })
-
+    );
   }
   @HostListener('window:keydown', ['$event'])
   onKeyPress($event: KeyboardEvent) {
     if (($event.ctrlKey || $event.metaKey) && $event.keyCode == 80) {
-      this.convetToPDF()
-      return false
+      this.convetToPDF();
+      return false;
     }
   }
   public convetToPDF() {
     const elementToPrint = document.getElementById('contentToConvert'); //The html element to become a pdf
     const pdf = new jspdf('p', 'mm', 'a4');
     pdf.addHTML(elementToPrint, () => {
-      pdf.save( this.dateOfPrint+'.pdf');
+      pdf.save(this.dateOfPrint + '.pdf');
     });
   }
   print() {
     var divToPrint = document.getElementById('contentToConvert');
-    var css = '@page { size: A4 landscape;color-adjust: exact;-webkit-print-color-adjust: exact; }',
+    var css =
+        '@page { size: A4 landscape;color-adjust: exact;-webkit-print-color-adjust: exact; }',
       style = document.createElement('style');
     style.type = 'text/css';
     style.media = 'print';
@@ -117,12 +142,15 @@ this.changeDeleiverMoneyForClient()
     divToPrint.appendChild(style);
     var newWin = window.open('', 'Print-Window');
     newWin?.document.open();
-    newWin?.document.write('<html dir="rtl"><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><link rel="stylesheet/less" type="text/css" href="app/reports/printpreview/agent/agent.component.less" /></head><body onload="window.print()">' + divToPrint?.innerHTML + '</body></html>');
+    newWin?.document.write(
+      '<html dir="rtl"><head><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"><link rel="stylesheet/less" type="text/css" href="app/reports/printpreview/agent/agent.component.less" /></head><body onload="window.print()">' +
+        divToPrint?.innerHTML +
+        '</body></html>'
+    );
     newWin?.document.close();
     setTimeout(function () {
       newWin?.close();
       // location.reload();
-
     }, 1000);
   }
 }
