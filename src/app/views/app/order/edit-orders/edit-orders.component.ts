ae1075eadd015426a62a550b8019359fd2ad23b2
bayan almouslly <bayan.almouslly@hotmail.com>
2023-05-03 20:20:35 +0200

    fix edit order

--- src/app/views/app/order/edit-orders/edit-orders.component.ts
+++ src/app/views/app/order/edit-orders/edit-orders.component.ts
@@ -218,7 +218,7 @@ export class EditOrdersComponent implements OnInit {
     }
 
     if (isNaN(this.Order.RegionId)) {
-      this.Order.RegionName = this.Order.RegionId.label;
+      this.Order.RegionName = this.Order?.RegionId?.label;
       this.Order.RegionId = null;
     }
     this.Order.DeliveryCost = Number(this.Order.DeliveryCost)
