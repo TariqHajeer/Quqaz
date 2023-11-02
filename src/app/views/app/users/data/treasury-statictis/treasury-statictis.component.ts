4008514d023d8a4c988a3e3b244477cc8beaa3be
bayan almouslly <bayan.almouslly@hotmail.com>
2023-11-02 22:45:35 +0200

    fix chart

--- src/app/views/app/users/data/treasury-statictis/treasury-statictis.component.ts
+++ src/app/views/app/users/data/treasury-statictis/treasury-statictis.component.ts
@@ -1,12 +1,9 @@
-import { Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, ViewChild, ViewEncapsulation } from '@angular/core';
+import { ChartComponent, IMouseEventArgs } from '@syncfusion/ej2-angular-charts';
 import * as moment from 'moment';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { TreasuryReportResponseDto } from 'src/app/Models/user/treasury.model';
 import { TreasuryService } from 'src/app/services/treasury.service';
-import { enableRtl } from '@syncfusion/ej2-base';
-
-enableRtl(true);
-
 
 @Component({
   selector: 'app-treasury-statictis',
@@ -21,10 +18,15 @@ export class TreasuryStatictisComponent implements OnChanges {
   toDate;
   public primaryXAxis?: Object;
   public chartData?: Object[] = [];
+  public chartDataCount?: Object[] = [];
+
   public title?: string;
   public primaryYAxis?: Object;
   public palette?: string[] = ["#E94649", "#F6B53F", "#6FAAB0", "#C4C24A"];
   tooltip
+  @ViewChild("chart") chart1?: ChartComponent;
+  public previousTarget = null;
+
   constructor(private treasuryService: TreasuryService,
     public spinner: NgxSpinnerService,
   ) { }
@@ -62,17 +64,26 @@ export class TreasuryStatictisComponent implements OnChanges {
           };
           this.tooltip = {
             enable: true,
-            format: '<b> ${point.x} : ${point.y}</b> ',
+            template: '<b style="direction: rtl !important;text-align: right;"> ${x} : ${y}</b> ',
           }
 
           this.chartData = [
-            { state: "العميل", amount: this.treasuryReport?.clientPayment?.amount, count: this.treasuryReport?.clientPayment?.count },
-            { state: "يجلب", amount: this.treasuryReport?.getReceipt?.amount, count: this.treasuryReport?.getReceipt?.count },
-            { state: "يعطي", amount: this.treasuryReport?.give?.amount, count: this.treasuryReport?.give?.count },
-            { state: "الواردات", amount: this.treasuryReport?.income?.amount, count: this.treasuryReport?.income?.count },
-            { state: "الصادرات", amount: this.treasuryReport?.outCome?.amount, count: this.treasuryReport?.outCome?.count },
-            { state: "يدفع", amount: this.treasuryReport?.payReceipt?.amount, count: this.treasuryReport?.payReceipt?.count },
-            { state: "يأخذ", amount: this.treasuryReport?.take?.amount, count: this.treasuryReport?.take?.count },
+            { state: "العميل", amount: this.treasuryReport?.clientPayment?.amount },
+            { state: "يجلب", amount: this.treasuryReport?.getReceipt?.amount },
+            { state: "يعطي", amount: this.treasuryReport?.give?.amount },
+            { state: "الواردات", amount: this.treasuryReport?.income?.amount },
+            { state: "الصادرات", amount: this.treasuryReport?.outCome?.amount },
+            { state: "يدفع", amount: this.treasuryReport?.payReceipt?.amount },
+            { state: "يأخذ", amount: this.treasuryReport?.take?.amount },
+          ];
+          this.chartDataCount = [
+            { state: "العميل", count: this.treasuryReport?.clientPayment?.count },
+            { state: "يجلب", count: this.treasuryReport?.getReceipt?.count },
+            { state: "يعطي", count: this.treasuryReport?.give?.count },
+            { state: "الواردات", count: this.treasuryReport?.income?.count },
+            { state: "الصادرات", count: this.treasuryReport?.outCome?.count },
+            { state: "يدفع", count: this.treasuryReport?.payReceipt?.count },
+            { state: "يأخذ", count: this.treasuryReport?.take?.count },
           ];
 
         }
@@ -81,6 +92,26 @@ export class TreasuryStatictisComponent implements OnChanges {
       })
     }
   }
+  public chartMouseClick(args: IMouseEventArgs): void {
+    var flag = false;
+    if (((args.target).indexOf('chart_legend_text') > -1) || ((args.target).indexOf('chart_legend_shape') > -1) ||
+      (args.target).indexOf('chart_legend_shape_marker_') && !(args.target).indexOf('chart_legend_element')) {
+      var ids = ((args.target).indexOf('chart_legend_text') > -1) ?
+        (args.target).split('chart_legend_text_')[1] : args.target.split('chart_legend_shape_marker_')[1] || args.target.split('chart_legend_shape_')[1];
+      for (var i = 0; i < this.chart1!.series.length; i++) {
+        this.chart1!.series[i].visible = false;
+      }
+      if (ids == this.previousTarget) {
+        for (var j = 0; j < this.chart1!.series.length; j++)
+          this.chart1!.series[j].visible = true;
+        this.chart1!.series[ids].visible = false;
+        this.previousTarget = null;
+        flag = true;
+      }
+      if (!flag)
+        this.previousTarget = ids as any;
+    }
+  };
   getTimeUTC(date) {
     return moment.utc(date).format();
   }
