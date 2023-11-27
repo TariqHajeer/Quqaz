d1678e710fae96d2b7df2b1763f4ec7c6b4343f3
bayan almouslly <bayan.almouslly@hotmail.com>
2023-11-27 22:06:53 +0200

    create client message

--- src/app/services/client-message.service.ts
+++ src/app/services/client-message.service.ts
@@ -10,16 +10,19 @@ export class ClientMessageService {
 
   controler = environment.baseUrl + "api/ClientMessage/";
   constructor(private http: HttpClient) { }
-  getClientMessage(paging: Paging) {
+  getClientMessage(paging: Paging, isPublished) {
     let params = new HttpParams();
     if (paging.Page)
-      params = params.append('page', paging.Page as any);
+      params = params.append('Page', paging.Page as any);
     if (paging.RowCount)
-      params = params.append('rowCount', paging.RowCount as any);
-    return this.http.get<any>("api/ClientMessage", { params: params });
+      params = params.append('RowCount', paging.RowCount as any);
+    if (isPublished)
+      params = params.append('isPublished', isPublished as any);
+
+    return this.http.get<any>(environment.baseUrl + "api/ClientMessage", { params: params });
   }
   deleteClientMessage(id: number) {
-    return this.http.get<any>(this.controler + "/" + id);
+    return this.http.delete<any>(this.controler + id);
   }
   publishClientMessage(id: number) {
     let params = new HttpParams();
--- src/app/views/app/setting/client-message/client-message.component.ts
+++ src/app/views/app/setting/client-message/client-message.component.ts
@@ -4,6 +4,7 @@ import { MatSort } from '@angular/material/sort';
 import { NotificationsService, NotificationType } from 'angular2-notifications';
 import { ClientMessageService } from 'src/app/services/client-message.service';
 import { Paging } from 'src/app/Models/paging';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-client-message',
@@ -17,23 +18,26 @@ export class ClientMessageComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   noDataFound: boolean = false;
   paging: Paging = new Paging();
+  isPublished: boolean = false;
   constructor(
     private notifications: NotificationsService,
     private clientMessageService: ClientMessageService
   ) { }
 
   ngOnInit(): void {
-    this.clientMessageService.getClientMessage(this.paging).subscribe({
+    this.getClientMessage();
+  }
+  getClientMessage() {
+    this.clientMessageService.getClientMessage(this.paging, this.isPublished).subscribe({
       next: (data) => {
-
+        this.dataSource = new MatTableDataSource(data.data);
       }
     })
   }
-
   Accept(id) {
     this.clientMessageService.publishClientMessage(id).subscribe({
       next: (data) => {
-
+        this.getClientMessage();
       }
     })
   }
@@ -41,14 +45,14 @@ export class ClientMessageComponent implements OnInit {
   DisAccept(id) {
     this.clientMessageService.unPublishClientMessage(id).subscribe({
       next: (data) => {
-
+        this.getClientMessage();
       }
     })
   }
   delete(id) {
     this.clientMessageService.deleteClientMessage(id).subscribe({
       next: (data) => {
-
+        this.getClientMessage();
       }
     })
   }
